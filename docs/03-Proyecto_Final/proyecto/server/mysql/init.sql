e812766259e51c1923e1f55ec1375d4621848cbf
Milena Lahiton <144371292+milelahiton@users.noreply.github.com>
2024-06-22 12:37:06 -0300

    Add files via upload

---
+++ docs/03-Proyecto_Final/proyecto/server/mysql/init.sql
@@ -0,0 +1,14 @@
+-- Crear la tabla mqtt_user
+CREATE TABLE `mqtt_user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password_hash` varchar(100) DEFAULT NULL,
+  `salt` varchar(35) DEFAULT NULL,
+  `is_superuser` tinyint(1) DEFAULT 0,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `mqtt_username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO mqtt_user(username, password_hash, salt, is_superuser)
+VALUES ('emqx_u', SHA2(concat('cliente_123', 'slat_foo123'), 256), 'slat_foo123', 1);
