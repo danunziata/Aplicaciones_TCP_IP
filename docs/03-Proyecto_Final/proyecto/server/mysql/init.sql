9521615df2052d84091fa1029d76814716d45a2b
andresbuten2002 <butenandres@gmail.com>
2024-06-25 19:43:55 -0300

    fix: Reemplazamos el init.sql que contiene los tres usuarios encriptados

--- docs/03-Proyecto_Final/proyecto/server/mysql/init.sql
+++ docs/03-Proyecto_Final/proyecto/server/mysql/init.sql
@@ -1,8 +1,11 @@
--- Crear la tabla mqtt_user
+CREATE DATABASE IF NOT EXISTS emqx_auth;
+
+USE emqx_auth;
+
 CREATE TABLE `mqtt_user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) DEFAULT NULL,
-  `password_hash` varchar(100) DEFAULT NULL,
+  `username` varchar(100) NOT NULL,
+  `password_hash` varchar(100) NOT NULL,
   `salt` varchar(35) DEFAULT NULL,
   `is_superuser` tinyint(1) DEFAULT 0,
   `created` datetime DEFAULT NULL,
@@ -10,5 +13,16 @@ CREATE TABLE `mqtt_user` (
   UNIQUE KEY `mqtt_username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO mqtt_user(username, password_hash, salt, is_superuser)
-VALUES ('emqx_u', SHA2(concat('cliente_123', 'slat_foo123'), 256), 'slat_foo123', 1);
+-- Insertar los usuarios con sus contraseñas en texto plano
+-- INSERT INTO mqtt_user (username, password_hash) VALUES ('sonoff', 'tcpip2024');
+-- INSERT INTO mqtt_user (username, password_hash) VALUES ('dht11', 'sebacrack');
+-- INSERT INTO mqtt_user (username, password_hash) VALUES ('raspy', 'tcpip2024');
+
+INSERT INTO mqtt_user (username, password_hash, salt) VALUES ('sonoff', SHA2(CONCAT('labiot2024', 'slat_foo123'), 256), 'slat_foo123');
+INSERT INTO mqtt_user (username, password_hash, salt) VALUES ('dht11', SHA2(CONCAT('sebacrack', 'slat_foo123'), 256), 'slat_foo123');
+INSERT INTO mqtt_user (username, password_hash, salt) VALUES ('raspy', SHA2(CONCAT('tcpip2024', 'slat_foo123'), 256), 'slat_foo123');
+
+-- Crear el usuario de base de datos y concederle permisos
+CREATE USER 'labiot'@'%' IDENTIFIED BY 'labiot2024';
+GRANT ALL PRIVILEGES ON emqx_auth.* TO 'labiot'@'%';
+FLUSH PRIVILEGES;
